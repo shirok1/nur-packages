ed06558241221065f5345f854c3d5cead076d347
Shiroki Satsuki <me@shirok1.dev>
2025-06-19 23:54:24 +0800

    feat: qbittorrent-clientblocker init

--- default.nix
+++ default.nix
@@ -15,6 +15,7 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
+  qbittorrent-clientblocker = pkgs.callPackage ./pkgs/qbittorrent-clientblocker { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
