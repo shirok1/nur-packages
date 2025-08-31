4e2d4ca96d5fb89cd6161c4d20b853fdf0e917ae
Shiroki Satsuki <me@shirok1.dev>
2025-08-31 14:46:47 +0800

    feat: use python3Packages to track distro Python

--- lib/default.nix
+++ lib/default.nix
@@ -4,8 +4,13 @@ with pkgs.lib; {
   # Add your library functions here
   #
   # hexint = x: hexvals.${toLower x};
-  fromJSON5File = json5File:
-    builtins.fromJSON (builtins.readFile (pkgs.runCommand "parsed.json" {
-      nativeBuildInputs = [ pkgs.python312Packages.json5 ];
-    } "pyjson5 --as-json ${json5File} > $out"));
+  fromJSON5File =
+    json5File:
+    builtins.fromJSON (
+      builtins.readFile (
+        pkgs.runCommand "parsed.json" {
+          nativeBuildInputs = [ pkgs.python3Packages.json5 ];
+        } "pyjson5 --as-json ${json5File} > $out"
+      )
+    );
 }
