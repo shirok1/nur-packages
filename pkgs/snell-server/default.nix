e73001bde1c2e905fa0aa861681c7eb35f5acc72
Shiroki Satsuki <me@shirok1.dev>
2025-09-06 17:07:12 +0800

    fix: sourceProvenance should be a list

--- pkgs/snell-server/default.nix
+++ pkgs/snell-server/default.nix
@@ -53,7 +53,7 @@ buildFHSEnv {
     description = "Snell is a lean encrypted proxy protocol developed by Surge team";
     homepage = "https://kb.nssurge.com/surge-knowledge-base/release-notes/snell";
     license = licenses.unfreeRedistributable;
-    sourceProvenance = sourceTypes.binaryNativeCode;
+    sourceProvenance = [ sourceTypes.binaryNativeCode ];
     platforms = builtins.attrNames platformMap;
     mainProgram = pname;
   };
