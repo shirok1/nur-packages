9c08f3330404fea3b4dff9e853626ac99a602f1e
Shiroki Satsuki <me@shirok1.dev>
2025-12-05 00:51:49 +0800

    chore: cleanup

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
   description = "My personal NUR repository";
-  #inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     daeuniverse.url = "github:daeuniverse/flake.nix";
